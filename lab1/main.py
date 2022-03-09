6a8f90052a0d2072afeb9d1701bc79c4d3ce9ca2
Argmaster <argmaster.world@gmail.com>
2022-03-09 12:54:57 +0100

    Initialize repo on 1st lab

---
+++ lab1/main.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def task_1():
+    # 1
+    A = np.linspace(1, 20, 10)
+    # 2
+    B = np.linspace(0, 1, 10)
+    # 3
+    AB = A * B
+    print(AB)
+    # 4
+    c = 2
+    Ac = A * c
+    print(Ac)
+    # 5
+    numpy
+
+
+
+
+
+
+if __name__ == "__main__":
+    task_1()
---
+++ lab1/notes.md
@@ -0,0 +1,6 @@
+# 09.03.2022
+
+# Homework
+
+Zebrać dane na dowolny temat
+Unikamy danych 0-1 (bo będą odbiegać od rozkładu normalnego)
