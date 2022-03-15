5bb3a725476dfb44984554bc16f007b3e9299662
Krzysztof Wiśniewski <argmaster.world@gmail.com>
2022-03-15 14:18:35 +0100

    Fix matrix ops, add project configs

--- lab1/main.py
+++ lab1/main.py
@@ -1,6 +1,12 @@
 import numpy as np
 
 
+def transpose(M: np.array) -> np.array:
+    return np.array(
+        [[M[i, j] for i in range(M.shape[0])] for j in range(M.shape[1])]
+    )
+
+
 def task_1():
     # 1
     A = np.linspace(1, 20, 10)
@@ -15,14 +21,14 @@ def task_1():
     print(Ac)
     # 5
     m4x4 = np.matrix(np.random.randint(1, 101, (4, 4)))
-    # Nie, macierze są tylko dwuwymiarowe, ndarray jest n-wymiarowa
+    # Nie, macierze są tylko dwuwymiarowe, ndarray może być n-wymiarowa
     print(m4x4)
     # 6
-    cross = np.array([m4x4[i, i] for i in range(4)])
-    print(cross)
+    print(np.array([m4x4[i, i] for i in range(4)]))
     # 7
-    transposed = [[e] for e in cross]
-    print(transposed)
+
+    print("m4x4:", m4x4, sep="\n")
+    print("m4x4_T:", transpose(m4x4), sep="\n")
 
 
 if __name__ == "__main__":
